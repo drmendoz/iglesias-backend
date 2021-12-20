29022ad522bf287c9ab663e578820b8ff6b449f2
galfeort <galfeort@espol.edu.ec>
2021-12-20 15:35:23 -0500

    Crear modelo parroquia

---
+++ models/parroquia.go
@@ -0,0 +1,34 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type Parroquia struct {
+	gorm.Model
+	Nombre                  string          `json:"nombre"`
+	Latitud                 float64         `json:"lat"`
+	Longitud                float64         `json:"lng"`
+	Direccion               string          `json:"direccion"`
+	Correo                  string          `json:"correo"`
+	Telefono                string          `json:"telefono"`
+	Imagen                  string          `json:"imagen"`
+	NombreBanco             string          `json:"nombre_banco"`
+	TipoCuenta              string          `json:"tipo_cuenta"`
+	NumeroCuenta            string          `json:"numero_cuenta"`
+	TipoDocumento           string          `json:"tipo_documento"`
+	NumeroDocumento         string          `json:"numero_documento"`
+	BotonPagoIntencion      bool            `json:"boton_pago_intencion"`
+	BotonPagoCurso          bool            `json:"boton_pago_curso"`
+	BotonPagoEmprendimiento bool            `json:"boton_pago_emprendimiento"`
+	BotonPagoMatrimonio     bool            `json:"boton_pago_matrimonio"`
+	CostoIntencion          float64         `json:"costo_intencion"`
+	CostoCurso              float64         `json:"costo_curso"`
+	CostoEmprendimiento     float64         `json:"costo_emprendimiento"`
+	CostoMatrimonio         float64         `json:"costo_matrimonio"`
+	Modulos                 ParroquiaModulo `json:"modulos"`
+}
+
+func (Parroquia) TableName() string {
+	return "parroquia"
+}
